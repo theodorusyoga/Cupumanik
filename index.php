e9a9338714086dc29295d362ff0d0bd3ba81f9d2
theodorusyoga <theodorusyoga_ti09@mail.ugm.ac.id>
2015-12-12 18:35:04 +0700

    ganti background putih abu2, tambah link2 di atas, ganti metode hover

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
 	integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
 	crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"
+	type="text/javascript"></script>
 <script
 	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
 	integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
@@ -18,10 +19,21 @@
 <body style="background-color: #000000">
 	<div class="header">
 		<div class="subheader">
-			<img class="logo" src="assets/logo-white.png" />
+			<img class="logo" src="assets/logo-black.png" />
 		</div>
 	</div>
+	<div class="menu">
+		<ul class="list">
+			<li><a class="link" href="/batik">ABOUT US</a></li>
+			<li>&nbsp;&nbsp;&nbsp;&nbsp; <a class="link" href="#">CONTACT US</a>
+			</li>
+			<li>&nbsp;&nbsp;&nbsp;&nbsp; <a class="link" href="#" target="_blank">FACEBOOK</a>
+			<li>&nbsp;&nbsp;&nbsp;&nbsp; <a class="link" href="#" target="_blank">TWITTER</a>&nbsp;&nbsp;&nbsp;&nbsp;
+			</li>
+		</ul>
+	</div>
 	<div class="main">
+		<div class="overlay"></div>
 		<span class="coverage">&nbsp;</span>
 		<div class="content">
 			<div class="submain">
@@ -36,19 +48,19 @@
 		</div>
 		<div class="preview">
 			<div class="previewcontent">&nbsp;</div>
-			<div class="images batiklink" >
-				 <div class="column">
-					<img id="img1" src="images/home.png" class="previewimg batiklink" />
+			<div class="images batiklink">
+				<div class="column">
+					<img id="img1" src="images/batik1.png" class="previewimg batiklink" />
 				</div>
 				<div class="column">
-					<img id="img2" src="images/home.png" class="previewimg batiklink" />
+					<img id="img2" src="images/batik2.png" class="previewimg batiklink" />
 				</div>
 				<div class="column">
-					<img id="img3" src="images/home.png" class="previewimg batiklink" />
-				</div>  
+					<img id="img3" src="images/batik3.png" class="previewimg batiklink" />
+				</div>
 			</div>
-			<div class="images furnilink" >
-				 <div class="column">
+			<div class="images furnilink">
+				<div class="column">
 					<img id="img1" src="images/home.png" class="previewimg furnilink" />
 				</div>
 				<div class="column">
@@ -56,28 +68,31 @@
 				</div>
 				<div class="column">
 					<img id="img3" src="images/home.png" class="previewimg furnilink" />
-				</div>  
+				</div>
 			</div>
-			<div class="images guesthouselink" >
-				 <div class="column">
-					<img id="img1" src="images/home.png" class="previewimg guesthouselink" />
+			<div class="images guesthouselink">
+				<div class="column">
+					<img id="img1" src="images/home.png"
+						class="previewimg guesthouselink" />
 				</div>
 				<div class="column">
-					<img id="img2" src="images/home.png" class="previewimg guesthouselink" />
+					<img id="img2" src="images/home.png"
+						class="previewimg guesthouselink" />
 				</div>
 				<div class="column">
-					<img id="img3" src="images/home.png" class="previewimg guesthouselink" />
-				</div>  
+					<img id="img3" src="images/home.png"
+						class="previewimg guesthouselink" />
+				</div>
 			</div>
 		</div>
 		<div class="foot">
-		<p>
-			Copyright &copy; 2015 <a
-				href="https://www.facebook.com/theodorus.yoga" target="_blank">T&S
-				Design and Program Team</a>
-		</p>
+			<p>
+				Copyright &copy; 2015 <a
+					href="https://www.facebook.com/theodorus.yoga" target="_blank">T&S
+					Design and Program Team</a>
+			</p>
+		</div>
 	</div>
-	</div>
-	
+
 </body>
 </html>
--- javascript/function.js
+++ javascript/function.js
@@ -1,22 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.batiklink').hide();
 	$('.furnilink').hide();
 	$('.guesthouselink').hide();
 	$('.preview').hide();
-	
-	$('.link').hover(function(){
+
+	window.setTimeout(onLoad, 750);
+
+	function onLoad() {
+		$('img.batiklink#img1').fadeIn(500);
+		$('img.batiklink#img2').fadeIn(1000);
+		$('img.batiklink#img3').fadeIn(1500);
+		$('.preview').fadeIn('fast');
+		$('div.batiklink').fadeIn('fast');
+	}
+
+	$('.submain .link').hover(function() {
+		//hide first
 		var id = this.id;
+		$('.batiklink').hide();
+		$('.furnilink').hide();
+		$('.guesthouselink').hide();
+		$('.preview').hide();
+		// show
 		$('img.' + id + '#img1').fadeIn(500);
 		$('img.' + id + '#img2').fadeIn(1000);
 		$('img.' + id + '#img3').fadeIn(1500);
 		$('.preview').fadeIn('fast');
 		$('div.' + id).fadeIn('fast');
-	}, function(){
+	}, function() {
 		var id = this.id;
+		//hide first
 		$('img.' + id + '#img1').hide();
 		$('img.' + id + '#img2').hide();
 		$('img.' + id + '#img3').hide();
 		$('.preview').hide();
 		$('div.' + id).hide();
-	})
+		//show
+		onLoad();
+	});
+
 });
